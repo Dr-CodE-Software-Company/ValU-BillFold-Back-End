e77cfe98c8acb2ef25427575949c6c3592bacea8
NashaatMohamed <khaled.shokery.969@gmail.com>
2023-06-20 17:13:47 +0200

    update

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -79,199 +79,8 @@
                     </li>
                     @endif
 
-                  {{-- <li class="sa-nav__menu-item">
-                    <a href="" class="sa-nav__link"
-                      ><span class="sa-nav__menu-item-padding"></span
-                      ><span class="sa-nav__title">{{__('message.Rooms')}}</span></a
-                    >
-                  </li> --}}
-{{--
-                   <li class="sa-nav__menu-item">
-                    <a href="{{url('Admin/users/notify')}}" class="sa-nav__link"
-                      ><span class="sa-nav__menu-item-padding"></span
-                      ><span class="sa-nav__title">{{__('message.Send_Notification')}}</span></a
-                    >
-                  </li> --}}
                 </ul>
               </li>
-
-              {{-- // start of background section --}}
-              {{-- <li
-                class="sa-nav__menu-item sa-nav__menu-item--has-icon"
-                data-sa-collapse-item="sa-nav__menu-item--open">
-                <a href="#" class="sa-nav__link" data-sa-collapse-trigger=""
-                  ><span class="sa-nav__icon"
-                    ><svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="1em"
-                      height="1em"
-                      viewBox="0 0 16 16"
-                      fill="currentColor"
-                    >
-                      <path
-                        d="M8,10c-3.3,0-6,2.7-6,6H0c0-3.2,1.9-6,4.7-7.3C3.7,7.8,3,6.5,3,5c0-2.8,2.2-5,5-5s5,2.2,5,5c0,1.5-0.7,2.8-1.7,3.7c2.8,1.3,4.7,4,4.7,7.3h-2C14,12.7,11.3,10,8,10z M8,2C6.3,2,5,3.3,5,5s1.3,3,3,3s3-1.3,3-3S9.7,2,8,2z"
-                      ></path></svg></span>
-                      <span class="sa-nav__title">{{__('message.Backgrounds')}}</span>
-                      <span class="sa-nav__arrow">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="6" height="9" viewBox="0 0 6 9" fill="currentColor">
-                      <path
-                        d="M5.605,0.213 C6.007,0.613 6.107,1.212 5.706,1.612 L2.696,4.511 L5.706,7.409 C6.107,7.809 6.107,8.509 5.605,8.808 C5.204,9.108 4.702,9.108 4.301,8.709 L-0.013,4.511 L4.401,0.313 C4.702,-0.087 5.304,-0.087 5.605,0.213 Z"></path></svg></span
-                ></a>
-                <ul class="sa-nav__menu sa-nav__menu--sub" data-sa-collapse-content="">
-                  <li class="sa-nav__menu-item">
-                    <a href="" class="sa-nav__link"><span class="sa-nav__menu-item-padding"></span>
-                        <span class="sa-nav__title">{{__('message.Backgrounds_list')}}</span>
-                    </a>
-                  </li>
-                  <li class="sa-nav__menu-item">
-                    <a href="" class="sa-nav__link"
-                      ><span class="sa-nav__menu-item-padding"></span
-                      ><span class="sa-nav__title">{{__('message.Backgrounds_create')}}</span></a
-                    >
-                  </li>
-                </ul>
-              </li> --}}
-                {{-- // end of background section --}}
-
-
-             {{-- // start of badges section --}}
-             {{-- <li
-             class="sa-nav__menu-item sa-nav__menu-item--has-icon"
-             data-sa-collapse-item="sa-nav__menu-item--open">
-             <a href="#" class="sa-nav__link" data-sa-collapse-trigger=""
-               ><span class="sa-nav__icon"
-                 ><svg
-                   xmlns="http://www.w3.org/2000/svg"
-                   width="1em"
-                   height="1em"
-                   viewBox="0 0 16 16"
-                   fill="currentColor"
-                 >
-                   <path
-                     d="M8,10c-3.3,0-6,2.7-6,6H0c0-3.2,1.9-6,4.7-7.3C3.7,7.8,3,6.5,3,5c0-2.8,2.2-5,5-5s5,2.2,5,5c0,1.5-0.7,2.8-1.7,3.7c2.8,1.3,4.7,4,4.7,7.3h-2C14,12.7,11.3,10,8,10z M8,2C6.3,2,5,3.3,5,5s1.3,3,3,3s3-1.3,3-3S9.7,2,8,2z"
-                   ></path></svg></span>
-                   <span class="sa-nav__title">{{__('message.Badges')}}</span>
-                   <span class="sa-nav__arrow">
-                 <svg xmlns="http://www.w3.org/2000/svg" width="6" height="9" viewBox="0 0 6 9" fill="currentColor">
-                   <path
-                     d="M5.605,0.213 C6.007,0.613 6.107,1.212 5.706,1.612 L2.696,4.511 L5.706,7.409 C6.107,7.809 6.107,8.509 5.605,8.808 C5.204,9.108 4.702,9.108 4.301,8.709 L-0.013,4.511 L4.401,0.313 C4.702,-0.087 5.304,-0.087 5.605,0.213 Z"></path></svg></span
-             ></a>
-             <ul class="sa-nav__menu sa-nav__menu--sub" data-sa-collapse-content="">
-               <li class="sa-nav__menu-item">
-                 <a href="" class="sa-nav__link"><span class="sa-nav__menu-item-padding"></span>
-                     <span class="sa-nav__title">{{__('message.Badge_list')}}</span>
-                 </a>
-               </li>
-               <li class="sa-nav__menu-item">
-                 <a href="" class="sa-nav__link">
-                <span class="sa-nav__menu-item-padding"></span>
-                <span class="sa-nav__title">{{__('message.Badge_create')}}</span>
-                </a>
-               </li>
-             </ul>
-           </li> --}}
-                         {{-- // end of badge section --}}
-
-
-                          {{-- // start of Music section --}}
-             {{-- <li
-             class="sa-nav__menu-item sa-nav__menu-item--has-icon"
-             data-sa-collapse-item="sa-nav__menu-item--open">
-             <a href="#" class="sa-nav__link" data-sa-collapse-trigger=""
-               ><span class="sa-nav__icon"
-                 ><svg
-                   xmlns="http://www.w3.org/2000/svg"
-                   width="1em"
-                   height="1em"
-                   viewBox="0 0 16 16"
-                   fill="currentColor"
-                 >
-                   <path
-                     d="M8,10c-3.3,0-6,2.7-6,6H0c0-3.2,1.9-6,4.7-7.3C3.7,7.8,3,6.5,3,5c0-2.8,2.2-5,5-5s5,2.2,5,5c0,1.5-0.7,2.8-1.7,3.7c2.8,1.3,4.7,4,4.7,7.3h-2C14,12.7,11.3,10,8,10z M8,2C6.3,2,5,3.3,5,5s1.3,3,3,3s3-1.3,3-3S9.7,2,8,2z"
-                   ></path></svg></span>
-                   <span class="sa-nav__title">{{__('message.music')}}</span>
-                   <span class="sa-nav__arrow">
-                 <svg xmlns="http://www.w3.org/2000/svg" width="6" height="9" viewBox="0 0 6 9" fill="currentColor">
-                   <path
-                     d="M5.605,0.213 C6.007,0.613 6.107,1.212 5.706,1.612 L2.696,4.511 L5.706,7.409 C6.107,7.809 6.107,8.509 5.605,8.808 C5.204,9.108 4.702,9.108 4.301,8.709 L-0.013,4.511 L4.401,0.313 C4.702,-0.087 5.304,-0.087 5.605,0.213 Z"></path></svg></span
-             ></a>
-             <ul class="sa-nav__menu sa-nav__menu--sub" data-sa-collapse-content="">
-               <li class="sa-nav__menu-item">
-                 <a href="" class="sa-nav__link"><span class="sa-nav__menu-item-padding"></span>
-                     <span class="sa-nav__title">{{__('message.music_list')}}</span>
-                 </a>
-               </li>
-               <li class="sa-nav__menu-item">
-                 <a href="" class="sa-nav__link"
-                   ><span class="sa-nav__menu-item-padding"></span
-                   ><span class="sa-nav__title">{{__('message.music_create')}}</span></a
-                 >
-               </li>
-             </ul>
-           </li> --}}
-                         {{-- // end of Music section --}}
-                {{-- /// start of section  --}}
-                {{-- <li
-                    class="sa-nav__menu-item sa-nav__menu-item--has-icon"
-                    data-sa-collapse-item="sa-nav__menu-item--open">
-                    <a href="#" class="sa-nav__link" data-sa-collapse-trigger=""
-                    ><span class="sa-nav__icon"
-                        ><svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="1em"
-                                height="1em"
-                                viewBox="0 0 16 16"
-                                fill="currentColor"
-                            >
-                   <path
-                       d="M8,10c-3.3,0-6,2.7-6,6H0c0-3.2,1.9-6,4.7-7.3C3.7,7.8,3,6.5,3,5c0-2.8,2.2-5,5-5s5,2.2,5,5c0,1.5-0.7,2.8-1.7,3.7c2.8,1.3,4.7,4,4.7,7.3h-2C14,12.7,11.3,10,8,10z M8,2C6.3,2,5,3.3,5,5s1.3,3,3,3s3-1.3,3-3S9.7,2,8,2z"
-                   ></path></svg></span>
-                        <span class="sa-nav__title">{{__('message.clans')}}</span>
-                        <span class="sa-nav__arrow">
-                 <svg xmlns="http://www.w3.org/2000/svg" width="6" height="9" viewBox="0 0 6 9" fill="currentColor">
-                   <path
-                       d="M5.605,0.213 C6.007,0.613 6.107,1.212 5.706,1.612 L2.696,4.511 L5.706,7.409 C6.107,7.809 6.107,8.509 5.605,8.808 C5.204,9.108 4.702,9.108 4.301,8.709 L-0.013,4.511 L4.401,0.313 C4.702,-0.087 5.304,-0.087 5.605,0.213 Z"></path></svg></span
-                        ></a>
-                    <ul class="sa-nav__menu sa-nav__menu--sub" data-sa-collapse-content="">
-                        <li class="sa-nav__menu-item">
-                            <a href="" class="sa-nav__link"><span class="sa-nav__menu-item-padding"></span>
-                                <span class="sa-nav__title">{{__('message.Clan_List')}}</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                /// end of section --}}
-                {{-- /// start of section  --}}
-                {{-- <li
-                    class="sa-nav__menu-item sa-nav__menu-item--has-icon"
-                    data-sa-collapse-item="sa-nav__menu-item--open">
-                    <a href="#" class="sa-nav__link" data-sa-collapse-trigger=""
-                    ><span class="sa-nav__icon"
-                        ><svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="1em"
-                                height="1em"
-                                viewBox="0 0 16 16"
-                                fill="currentColor"
-                            >
-                   <path
-                       d="M8,10c-3.3,0-6,2.7-6,6H0c0-3.2,1.9-6,4.7-7.3C3.7,7.8,3,6.5,3,5c0-2.8,2.2-5,5-5s5,2.2,5,5c0,1.5-0.7,2.8-1.7,3.7c2.8,1.3,4.7,4,4.7,7.3h-2C14,12.7,11.3,10,8,10z M8,2C6.3,2,5,3.3,5,5s1.3,3,3,3s3-1.3,3-3S9.7,2,8,2z"
-                   ></path></svg></span>
-                        <span class="sa-nav__title">{{__('message.Rooms')}}</span>
-                        <span class="sa-nav__arrow">
-                 <svg xmlns="http://www.w3.org/2000/svg" width="6" height="9" viewBox="0 0 6 9" fill="currentColor">
-                   <path
-                       d="M5.605,0.213 C6.007,0.613 6.107,1.212 5.706,1.612 L2.696,4.511 L5.706,7.409 C6.107,7.809 6.107,8.509 5.605,8.808 C5.204,9.108 4.702,9.108 4.301,8.709 L-0.013,4.511 L4.401,0.313 C4.702,-0.087 5.304,-0.087 5.605,0.213 Z"></path></svg></span
-                        ></a>
-                    <ul class="sa-nav__menu sa-nav__menu--sub" data-sa-collapse-content="">
-                        <li class="sa-nav__menu-item">
-                            <a href="" class="sa-nav__link"><span class="sa-nav__menu-item-padding"></span>
-                                <span class="sa-nav__title">{{__('message.rooms_List')}}</span>
-                            </a>
-                        </li>
-                    </ul>
-                </li> --}}
-                {{-- /// end of section --}}
                 <li
                     class="sa-nav__menu-item sa-nav__menu-item--has-icon"
                     data-sa-collapse-item="sa-nav__menu-item--open">
@@ -336,7 +145,7 @@
                                     <span class="sa-nav__title">{{__('message.Subscription_create')}}</span>
                                 </a>
                             </li>
-                        @endcan
+                        @endif
                     </ul>
                 </li>
                 {{-- /// end of section --}}
@@ -358,14 +167,12 @@
                       d="M5.605,0.213 C6.007,0.613 6.107,1.212 5.706,1.612 L2.696,4.511 L5.706,7.409 C6.107,7.809 6.107,8.509 5.605,8.808 C5.204,9.108 4.702,9.108 4.301,8.709 L-0.013,4.511 L4.401,0.313 C4.702,-0.087 5.304,-0.087 5.605,0.213 Z"></path></svg></span
               ></a>
               <ul class="sa-nav__menu sa-nav__menu--sub" data-sa-collapse-content="">
-{{--                  @if(auth('admin')->user()->hasRole('admin'))--}}
                       @if(auth('admin')->user()->can('Role-List'))
                       <li class="sa-nav__menu-item">
                     <a href="{{url('Admin/Role')}}" class="sa-nav__link"><span class="sa-nav__menu-item-padding"></span>
                         <span class="sa-nav__title">{{__('message.Roles_list')}}</span>
                     </a>
                 </li>
-
                   @endif
                   @if(auth('admin')->user()->can('Role-Create'))
                   <li class="sa-nav__menu-item">
@@ -374,7 +181,7 @@
                     <span class="sa-nav__title">{{__('message.Roles_create')}}</span>
                 </a>
                 </li>
-                  @endcan
+                  @endif
               </ul>
             </li>
             {{-- /// end of section --}}
