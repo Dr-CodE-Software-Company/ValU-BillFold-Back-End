566d03bcb120d2bc4a17392a588527d825568269
NashaatMohamed <khaled.shokery.969@gmail.com>
2023-06-21 23:55:15 +0200

    update

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -81,7 +81,6 @@ class AuthController extends Controller
     }
 
     public function login(Request $request){
-        try {
             $validator = Validator::make($request->all(), [
                 'email' => 'required|email',
                 'password' => 'required',
@@ -103,10 +102,7 @@ class AuthController extends Controller
             }else{
                 return Response::json(['status'=>false,'message'=> 'Email and Password are Wrong.'],400);
             }
-        }catch (Exception $e){
-            return Response::json(['status'=>false,'message'=>'Server Error','error'=>$e->getMessage()],500);
         }
-    }
 
     public function ForgetPasswordUser(Request $request){
         $user = User::where("email",$request->email)->get();
